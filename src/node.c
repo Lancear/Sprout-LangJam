15a4a2a35efaace8a6ac588c4009709516da3d8e
Kamila Szewczyk <kspalaiologos@gmail.com>
2020-08-20 21:26:36 +0200

    remove the const qualifier

--- src/node.c
+++ src/node.c
@@ -1,8 +1,10 @@
 
 #include <assert.h>
+#include <string.h>
+#include <stdlib.h>
 #include "node.h"
 
-struct node * new_node(unsigned int type, struct node * child, struct node * brother, const char * value) {
+struct node * new_node(unsigned int type, struct node * child, struct node * brother, char * value) {
     struct node * instance = malloc(sizeof(struct node));
     assert(instance);
     
