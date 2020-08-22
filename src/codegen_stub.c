df522cdd39e02959700bbfa32ad8c42fe51e1480
Kamila Szewczyk <kspalaiologos@gmail.com>
2020-08-22 21:51:29 +0200

    filter out all the garbage from the output

--- src/codegen_stub.c
+++ src/codegen_stub.c
@@ -12,9 +12,7 @@ void dispatch(struct node * n) {
         return;
     
     INDENT; printf("type: %d\n", n->type);
-    INDENT; printf("value: %s\n", n->value);
-    if(!n->child) { INDENT; printf("child: (null)\n"); }
-    else { INDENT; indent += 1; printf("child:\n"); dispatch(n->child); indent -= 1; }
-    if(!n->brother) { INDENT; printf("brother: (null)\n"); }
-    else { INDENT; indent += 1; printf("brother:\n"); dispatch(n->brother); indent -= 1; }
+    if(n->value != NULL) { INDENT; printf("value: %s\n", n->value); }
+    if(n->child)   { INDENT; indent += 1; printf("child:\n"); dispatch(n->child); indent -= 1; }
+    if(n->brother) { INDENT; indent += 1; printf("brother:\n"); dispatch(n->brother); indent -= 1; }
 }
