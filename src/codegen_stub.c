ac75a6b59abc5c7abfb64f3359229d92917f029b
Kamila Szewczyk <kspalaiologos@gmail.com>
2020-08-23 20:40:49 +0200

    parser roadmap

--- src/codegen_stub.c
+++ src/codegen_stub.c
@@ -13,6 +13,6 @@ void dispatch(struct node * n) {
     
     INDENT; printf("type: %d\n", n->type);
     if(n->value != NULL) { INDENT; printf("value: %s\n", n->value); }
-    if(n->child)   { INDENT; indent += 1; printf("child:\n"); dispatch(n->child); indent -= 1; }
-    if(n->brother) { INDENT; indent += 1; printf("brother:\n"); dispatch(n->brother); indent -= 1; }
+    if(n->child)         { INDENT; indent += 1; printf("child:\n"); dispatch(n->child); indent -= 1; }
+    if(n->brother)       { INDENT; indent += 1; printf("brother:\n"); dispatch(n->brother); indent -= 1; }
 }
