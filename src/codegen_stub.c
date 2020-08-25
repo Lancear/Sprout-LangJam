18d7774d311c3eeb95263c940ae8287f825a0d0f
Kamila Szewczyk <kspalaiologos@gmail.com>
2020-08-25 19:43:08 +0200

    sanctify

--- src/codegen_stub.c
+++ src/codegen_stub.c
@@ -12,7 +12,7 @@ void dispatch(struct node * n) {
         return;
     
     INDENT; printf("type: %d\n", n->type);
-    if(n->value != NULL) { INDENT; printf("value: %s\n", n->value); }
-    if(n->child)         { INDENT; indent += 1; printf("->\n"); dispatch(n->child); indent -= 1; }
-    if(n->brother)       { INDENT; printf("----\n"); dispatch(n->brother); }
+    if(n->value)    { INDENT; printf("value: %s\n", n->value); }
+    if(n->child)    { INDENT; indent += 1; printf("->\n"); dispatch(n->child); indent -= 1; }
+    if(n->brother)  { INDENT; printf("----\n"); dispatch(n->brother); }
 }
