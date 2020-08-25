7f18bb9dfaf9a8ece180b01db8f668dd019f21be
Lukas Schaffer <45712080+lukas-schaffer@users.noreply.github.com>
2020-08-25 21:39:14 +0200

    added self written code

--- src/interpreter/nodes/TreeNode.cpp
+++ src/interpreter/nodes/TreeNode.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 
 #include "TreeNode.hpp"
-#include "ArithmeticExpressionNode.hpp"
-#include "CodeBlockNode.hpp"
+#include "ImportDeclarationNode.hpp"
+#include "AsCompoundNode.hpp"
 #include "FunctionDeclarationNode.hpp"
-#include "FunctionTypeNode.hpp"
-#include "ParameterNode.hpp"
 #include "ParameterListNode.hpp"
+#include "ParameterNode.hpp"
 #include "ParameterTypeNode.hpp"
-#include "ReturnStatementNode.hpp"
+#include "CodeBlockNode.hpp"
 #include "StatementListNode.hpp"
+#include "ReturnStatementNode.hpp"
+#include "NumericImmediateNode.hpp"
+#include "FunctionTypeNode.hpp"
+#include "LocalDeclarationStatementNode.hpp"
+#include "MutableLocalDeclarationStatementNode.hpp"
+#include "VariableTypeNodeNode.hpp"
+#include "ConditionalStatementNode.hpp"
+#include "ElseCompoundNode.hpp"
+#include "TypeCompoundNode.hpp"
+#include "StringImmediateNode.hpp"
+#include "ComparisonExpresisonNode.hpp"
+#include "WhileStatementNode.hpp"
+#include "ForStatementNode.hpp"
+#include "ModuleDeclarationNode.hpp"
+#include "AssignmentExpressionNode.hpp"
+#include "FunctionCallNode.hpp"
+#include "LValueNode.hpp"
+#include "DoWhileStatementNode.hpp"
+#include "PostOpNode.hpp"
+#include "PreOpNode.hpp"
+#include "ClassDeclarationNode.hpp"
+#include "ArithmeticExpressionNode.hpp"
+#include "TenaryNode.hpp"
 #include "../../parser/node.h"
 #include "../symboltable/Symbol.hpp"
 
@@ -65,8 +87,74 @@ unique_ptr<TreeNode> TreeNode::of(struct node * n) {
 
     case StatementList:
       return make_unique<StatementListNode>(n);
-    
-    default:
+
+	case ImportDeclaration:
+		return make_unique<ImportDeclarationNode>(n);
+
+	case AsCompound:
+		return make_unique<AsCompoundNode>(n);
+
+	case NumericImmediate:
+		return make_unique<NumericImmediateNode>(n);
+
+	case LocalDeclarationStatement:
+		return make_unique<LocalDeclarationStatementNode>(n);
+
+	case MutableLocalDeclarationStatement:
+		return make_unique<MutableLocalDeclarationStatementNode>(n);
+
+	case VariableTypeNode:
+		return make_unique<VariableTypeNodeNode>(n);
+
+	case ConditionalStatement:
+		return make_unique<ConditionalStatementNode>(n);
+
+	case ElseCompound:
+		return make_unique<ElseCompoundNode>(n);
+
+	case TypeCompound:
+		return make_unique<TypeCompoundNode>(n);
+
+	case StringImmediate:
+		return make_unique<StringImmediateNode>(n);
+
+	case ComparisonExpresison:
+		return make_unique<ComparisonExpresisonNode>(n);
+
+	case WhileStatement:
+		return make_unique<WhileStatementNode>(n);
+
+	case ForStatement:
+		return make_unique<ForStatementNode>(n);
+
+	case ModuleDeclaration:
+		return make_unique<ModuleDeclarationNode>(n);
+
+	case AssignmentExpression:
+		return make_unique<AssignmentExpressionNode>(n);
+
+	case FunctionCall:
+		return make_unique<FunctionCallNode>(n);
+
+	case LValue:
+		return make_unique<LValueNode>(n);
+
+	case DoWhileStatement:
+		return make_unique<DoWhileStatementNode>(n);
+
+	case PostOp:
+		return make_unique<PostOpNode>(n);
+
+	case PreOp:
+		return make_unique<PreOpNode>(n);
+
+	case ClassDeclaration:
+		return make_unique<ClassDeclarationNode>(n);
+
+	case Tenary:
+		return make_unique<TenaryNode>(n);
+
+	default:
       cout << "ERRORROR: " << n->type << endl;
       return nullptr;
   }
