6571bdb586cff2560819c89ac92b5660f4a7fd33
Kamila Szewczyk <kspalaiologos@gmail.com>
2020-08-25 18:54:15 +0200

    blep

--- src/node.h
+++ src/node.h
@@ -11,7 +11,7 @@ enum {
     ForStatement, ModuleDeclaration, AssignmentExpression, FunctionCall, LValue, DoWhileStatement,
     /* currently I'm the second one */
     PostOp, PreOp,
-    ClassDeclaration
+    ClassDeclaration, Tenary
 };
 
 struct node {
